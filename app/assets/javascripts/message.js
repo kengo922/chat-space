6a92f7decd9d44613fc502abae6327e02aaf5bb7
kengo922 <kengo.matsuno.922@gmail.com>
2020-04-03 21:24:31 +0900

    自動更新機能 V2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,12 +52,19 @@ $(function(){
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('form')[0].reset();
+      .done(function(data){
+        console.log("hoge")
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('form')[0].reset();
+        $('.submit-btn').prop('disabled', false);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});  
+      }) 
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+      });
     })
-  })
+  
 
     var reloadMessages = function() {
       var last_message_id = $('.message:last').data("message-id");
@@ -85,6 +92,6 @@ $(function(){
         });
     };
     if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-      setInterval(reloadMessages, 3000);
+      setInterval(reloadMessages, 7000);
     }
-  });
+});
